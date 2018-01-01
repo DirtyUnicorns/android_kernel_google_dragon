34fcec13849a9a3a86cbb9592d693dbeecdae7d5
Vartom <babanakov_dima@mail.ru>
2018-01-01 18:14:32 +0700

    Nouveau: Reduce spam in the log.

--- drivers/gpu/drm/nouveau/nouveau_drm.c
+++ drivers/gpu/drm/nouveau/nouveau_drm.c
@@ -584,20 +584,20 @@ nouveau_do_suspend(struct drm_device *dev, bool runtime)
 	}
 
 	if (dev->mode_config.num_crtc) {
-		NV_INFO(drm, "suspending console...\n");
+//		NV_INFO(drm, "suspending console...\n");
 		nouveau_fbcon_set_suspend(dev, 1);
-		NV_INFO(drm, "suspending display...\n");
+//		NV_INFO(drm, "suspending display...\n");
 		ret = nouveau_display_suspend(dev, runtime);
 		if (ret)
 			return ret;
 	}
 
 	if (dev->pdev) {
-		NV_INFO(drm, "evicting buffers...\n");
+//		NV_INFO(drm, "evicting buffers...\n");
 		ttm_bo_evict_mm(&drm->ttm.bdev, TTM_PL_VRAM);
 	}
 
-	NV_INFO(drm, "waiting for kernel channels to go idle...\n");
+//	NV_INFO(drm, "waiting for kernel channels to go idle...\n");
 	if (drm->cechan) {
 		ret = nouveau_channel_idle_suspend(drm->cechan);
 		if (ret)
@@ -610,7 +610,7 @@ nouveau_do_suspend(struct drm_device *dev, bool runtime)
 			goto fail_display;
 	}
 
-	NV_INFO(drm, "waiting for client channels to go idle...\n");
+//	NV_INFO(drm, "waiting for client channels to go idle...\n");
 	list_for_each_entry(cli, &drm->clients, head) {
 		/*
 		 * XXX
@@ -648,7 +648,7 @@ nouveau_do_suspend(struct drm_device *dev, bool runtime)
 		mutex_unlock(&cli->mutex);
 	}
 
-	NV_INFO(drm, "suspending client object trees...\n");
+//	NV_INFO(drm, "suspending client object trees...\n");
 	if (drm->fence && nouveau_fence(drm)->suspend) {
 		if (!nouveau_fence(drm)->suspend(drm)) {
 			ret = -ENOMEM;
@@ -662,14 +662,14 @@ nouveau_do_suspend(struct drm_device *dev, bool runtime)
 			goto fail_client;
 	}
 
-	NV_INFO(drm, "suspending kernel object tree...\n");
+//	NV_INFO(drm, "suspending kernel object tree...\n");
 	ret = nvif_client_suspend(&drm->client.base);
 	if (ret)
 		goto fail_client;
 
 	nouveau_agp_fini(drm);
 
-	NV_INFO(drm, "nouveau suspended\n");
+//	NV_INFO(drm, "nouveau suspended\n");
 	return 0;
 
 fail_client:
@@ -682,10 +682,10 @@ fail_client:
 
 fail_display:
 	if (dev->mode_config.num_crtc) {
-		NV_INFO(drm, "resuming display...\n");
+//		NV_INFO(drm, "resuming display...\n");
 		nouveau_display_resume(dev, runtime);
 	}
-	NV_ERROR(drm, "suspend aborted!\n");
+//	NV_ERROR(drm, "suspend aborted!\n");
 	return ret;
 }
 
@@ -695,15 +695,15 @@ nouveau_do_resume(struct drm_device *dev, bool runtime)
 	struct nouveau_drm *drm = nouveau_drm(dev);
 	struct nouveau_cli *cli;
 
-	NV_INFO(drm, "re-enabling device...\n");
+//	NV_INFO(drm, "re-enabling device...\n");
 
 	nouveau_agp_reset(drm);
 
-	NV_INFO(drm, "resuming kernel object tree...\n");
+//	NV_INFO(drm, "resuming kernel object tree...\n");
 	nvif_client_resume(&drm->client.base);
 	nouveau_agp_init(drm);
 
-	NV_INFO(drm, "resuming client object trees...\n");
+//	NV_INFO(drm, "resuming client object trees...\n");
 	if (drm->fence && nouveau_fence(drm)->resume)
 		nouveau_fence(drm)->resume(drm);
 
@@ -715,13 +715,13 @@ nouveau_do_resume(struct drm_device *dev, bool runtime)
 		nouveau_run_vbios_init(dev);
 
 	if (dev->mode_config.num_crtc) {
-		NV_INFO(drm, "resuming display...\n");
+//		NV_INFO(drm, "resuming display...\n");
 		nouveau_display_resume(dev, runtime);
-		NV_INFO(drm, "resuming console...\n");
+//		NV_INFO(drm, "resuming console...\n");
 		nouveau_fbcon_set_suspend(dev, 0);
 	}
 
-	NV_INFO(drm, "nouveau resumed\n");
+//	NV_INFO(drm, "nouveau resumed\n");
 	return 0;
 }
 
