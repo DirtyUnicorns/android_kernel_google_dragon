81ca4ee076ea2e265d767861624d660a8f37354c
Nathan Chancellor <natechancellor@gmail.com>
2018-09-24 23:18:55 -0700

    BACKPORT: FROMLIST: pinctrl: generic: Avoid several implicit enum conversions
    Clang warns when one enumerated type is implicitly converted to another,
    which happens several times in the pinctrl drivers for a few reasons:
    
    * The PCONFDUMP macro, which sets the param member in pin_config_item.
    * The pinconf_generic_params structure, which is used by drivers to
      configure their bindings, which has a param member like pin_config_item.
    * The pinconf_to_config_packed, which takes either the generic enum
      pin_config_param or a specialized one.
    
    Drivers are allowed to extend this enumerated type because of the gap
    betweem PIN_CONFIG_END and PIN_CONFIG_MAX. Make it clear to Clang that
    this is allowed by changing param's type in all of these instances to
    int so no conversion needs to happen.
    
    Link: https://github.com/ClangBuiltLinux/linux/issues/138
    (am from https://lore.kernel.org/lkml/20180925061855.19557-1-natechancellor@gmail.com/raw)
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

--- include/linux/pinctrl/pinconf-generic.h
+++ include/linux/pinctrl/pinconf-generic.h
@@ -120,7 +120,7 @@ enum pin_config_param {
 				.has_arg = d }
 
 struct pin_config_item {
-	const enum pin_config_param param;
+	const int param;
 	const char * const display;
 	const char * const format;
 	bool has_arg;
@@ -149,7 +149,7 @@ static inline u16 pinconf_to_config_argument(unsigned long config)
 	return (enum pin_config_param) ((config >> 16) & 0xffffUL);
 }
 
-static inline unsigned long pinconf_to_config_packed(enum pin_config_param param,
+static inline unsigned long pinconf_to_config_packed(int param,
 						     u16 argument)
 {
 	return PIN_CONF_PACKED(param, argument);
@@ -164,7 +164,7 @@ struct pinctrl_map;
 
 struct pinconf_generic_dt_params {
 	const char * const property;
-	enum pin_config_param param;
+	int param;
 	u32 default_value;
 };
 
